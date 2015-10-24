579644ac76107b6d7277d6bf002474013f29eb17
dd0125 <dd.0125.jp@gmail.com>
2015-10-24 23:21:45 +0900

    ReadMe更新

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # terminal_config
+
+最初に流すもの
+
+```shell
+git clone --depth 1 https://github.com/dd0125/terminal_config.git ./
+rm -fr ./.git
+rm README.md
+
+sh init_script/init_centos_standard.sh
+```
---
+++ init_centos_standard.sh
@@ -0,0 +1,12 @@
+# とりあえず yum をアップデート
+sudo yum update
+
+# 基本的に必要なパッケージをインストール
+sudo yum -y install vim
+sudo yum -y install git
+sudo yum -y install zsh
+sudo yum -y install wget
+sudo yum -y install man
+sudo yum -y install make
+sudo yum -y install jwhois
+
